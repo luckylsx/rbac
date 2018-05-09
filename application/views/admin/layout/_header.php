3ebd2be228e164e7e54e0d323f5b6bf3f91c8928
lucky.li <lucky.li@etocrm.net>
2018-05-09 12:57:25 +0800

    fix the top nav

--- application/views/admin/layout/_header.php
+++ application/views/admin/layout/_header.php
@@ -1,25 +1,28 @@
-<style type="text/css">
-    .dropDown_hover .dropDown_A a:hover{
-        opacity: 1;
-        background: black;
-    }
-</style>
 <header class="navbar-wrapper">
-	<div class="navbar navbar-fixed-top">
-		<div class="container-fluid cl">
-            <a class="logo navbar-logo f-l mr-10 hidden-xs" href="javascript:void(0)">目录册</a>
-            <span class="logo navbar-slogan f-l mr-10 hidden-xs">后台</span>
+    <div class="navbar navbar-fixed-top">
+        <div class="container-fluid cl">
+            <a class="logo navbar-logo f-l mr-10 hidden-xs" href="javascript:void(0)">RBAC</a>
+            <span class="logo navbar-slogan f-l mr-10 hidden-xs">权限系统</span>
             <a aria-hidden="false" class="nav-toggle Hui-iconfont visible-xs" href="javascript:;">&#xe667;</a>
-			<nav id="Hui-userbar" class="nav navbar-nav navbar-userbar hidden-xs">
-				<ul class="cl">
-					<li class="">
-                            <?php echo isset($user['username'])&&!empty($user['username']) ? $user['username']:"";?><i class="Hui-iconfont">&#xe6d5;</i>
-						<ul class="dropDown-menu menu radius" style="border-radius: 5px">
-							<li><a href="<?php  echo site_url('admin/login/logout') ?>">退出</a></li>
-						</ul>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	</div>
+            <nav id="Hui-userbar" class="nav navbar-nav navbar-userbar hidden-xs">
+                <ul class="cl">
+                    <li class="dropDown dropDown_hover"> <a href="#" class="dropDown_A" style="background: none"><?php echo isset($user['username'])&&!empty($user['username']) ? $user['username']:"";?><i class="Hui-iconfont"> &#xe6d5;</i></a>
+                        <ul class="dropDown-menu menu radius box-shadow">
+                            <li class="logout"><a href="<?php  echo site_url('admin/login/logout') ?>">退出</a></li>
+                        </ul>
+                    </li>
+                    <li id="Hui-skin" class="dropDown right dropDown_hover"> <a href="javascript:;" class="dropDown_A" title="换肤"><i class="Hui-iconfont" style="font-size:18px">&#xe62a;</i></a>
+                        <ul class="dropDown-menu menu radius box-shadow">
+                            <li><a href="javascript:;" data-val="default" title="默认（黑色）">默认（黑色）</a></li>
+                            <li><a href="javascript:;" data-val="blue" title="蓝色">蓝色</a></li>
+                            <li><a href="javascript:;" data-val="green" title="绿色">绿色</a></li>
+                            <li><a href="javascript:;" data-val="red" title="红色">红色</a></li>
+                            <li><a href="javascript:;" data-val="yellow" title="黄色">黄色</a></li>
+                            <li><a href="javascript:;" data-val="orange" title="橙色">橙色</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </header>
